5c2a663be51be004b9cc43b44d8826680705cd3f
Likhitha26 <likhithakorrapati@gmail.com>
2018-07-02 16:37:50 -0100

    modified checker_34.c according to standard opcodes

--- c_files/checker_64.c
+++ c_files/checker_64.c
@@ -8,7 +8,7 @@ unsigned long long checker_64(unsigned char opcode, unsigned char funct3, unsign
   unsigned int  y = imm & 0xc00;
   unsigned long long t = 0x1;
 
-  if(opcode == 0 && (((funct3 == 3|| funct3 == 4) && (y == 0x800 || y == 0xc00)) || funct3 == 5))
+  if(opcode == 0x13 && (((funct3 == 3|| funct3 == 4) && (y == 0x800 || y == 0xc00)) || funct3 == 5))
 		shamt = imm & (63);
   else if(opcode == 1 && funct3 == 3 && y == 0x800)
     shamt = 0x3f; //cbrev
@@ -21,14 +21,14 @@ unsigned long long checker_64(unsigned char opcode, unsigned char funct3, unsign
   if(opcode == 1 && funct3 == 3 && y == 0x000) //cneg
     return((~rs1) + 1);
 
-  if(opcode == 0 && funct3 == 0) //clz
+  if(opcode == 0x1b && funct3 == 0) //clzw
     {for (int count = 0; count < 64; count++)
 			{if ((rs1 << count) >> (63))
 				{return count;}}
 		return 64;
 		}
 
-	if(opcode == 0 && funct3 == 1) //ctz
+	if(opcode == 0x1b && funct3 == 1) //ctzw
 		{for (int count = 0; count < 64; count++)
 			{if ((rs1 >> count) & 1)
 				{return count;}}
@@ -37,7 +37,7 @@ unsigned long long checker_64(unsigned char opcode, unsigned char funct3, unsign
 
   unsigned long long rd = 0; 
 
-  if(opcode == 0 && funct3 == 2)//pcnt
+  if(opcode == 0x1b && funct3 == 2)//pcntw
     {
       int count = 0;
       for (int index = 0; index < 64; index++)
@@ -45,22 +45,22 @@ unsigned long long checker_64(unsigned char opcode, unsigned char funct3, unsign
       rd = count;
     }
 
-  if(opcode == 4 && funct3 == 0)//andc
+  if(opcode == 0x33 && funct3 == 0)//andc
     rd = rs1 & ~rs2;
 
-  if((opcode == 4 && funct3 == 1 && y == 0x800) || (opcode == 0 && funct3 == 4 && y == 0x800))//sro sroi
+  if((opcode == 0x3b && funct3 == 0) || (opcode == 0x1b && funct3 == 4))//srow sroiw
     rd = (~(~rs1 >> shamt));
 
-  if((opcode == 4 && funct3 == 2 && y == 0x800) || (opcode == 0 && funct3 == 3 && y == 0x800))//slo sloi
+  if((opcode == 0x3b && funct3 == 1) || (opcode == 0x1b && funct3 == 3))//slow sloiw
     rd = (~(~rs1 << shamt));
 
-  if((opcode == 4 && funct3 == 1 && y == 0xc00) || (opcode == 0 && funct3 == 3 && y == 0xc00))//ror rori
+  if((opcode == 0x3b && funct3 == 2) || (opcode == 0x1b && funct3 == 5))//rorw roriw
     rd = ((rs1 >> shamt) | (rs1 << (64 - shamt)));
 
-  if(opcode == 4 && funct3 == 2 && y == 0xc00)//rol
+  if(opcode == 0x3b && funct3 == 3)//rolw
     rd = ((rs1 << shamt) | (rs1 >> (64 - shamt)));
 
-  if((opcode == 4 && funct3 == 3)||(opcode == 0 && funct3 == 5)||(opcode == 1 && funct3 == 3 && y == 0x800))//grev grevi
+  if((opcode == 0x33 && funct3 == 3)||(opcode == 0x13 && funct3 == 5)||(opcode == 1 && funct3 == 3 && y == 0x800))//grev grevi
     {
 
       if(shamt & 1)
@@ -79,7 +79,7 @@ unsigned long long checker_64(unsigned char opcode, unsigned char funct3, unsign
       rd = x;
     }
 
-  if(opcode == 0 && funct3 == 6)// gzip 
+  if(opcode == 0x13 && funct3 == 6)// gzip 
     {
       if(shamt & 1)
         {
@@ -112,7 +112,7 @@ unsigned long long checker_64(unsigned char opcode, unsigned char funct3, unsign
       rd = x;
     }
 
-  if(opcode == 4 && funct3 == 4)//bit extract
+  if(opcode == 0x3b && funct3 == 4)//bit extract
     {
      unsigned long long r = 0;
      for (int i = 0, j = 0; i < 64; i++)
@@ -125,7 +125,7 @@ unsigned long long checker_64(unsigned char opcode, unsigned char funct3, unsign
      rd = r;
     }
 
-  else if(opcode == 4 && funct3 == 5)//bit deposit
+  else if(opcode == 0x3b && funct3 == 5)//bit deposit
     {
       unsigned long long r = 0;
       for (int i = 0, j = 0; i < 64; i++)
